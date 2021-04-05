3dc2277ea8d5b26a144d6348c1b6866c3f0c8b05
gerudaeta <gerudaeta@gmail.com>
2021-04-05 18:01:10 -0300

    + Creacion entidad Usuario y Direccion

---
+++ Core/Entities/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Core.Entities
+{
+    public class Usuario : IdentityUser
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public Direccion Direccion { get; set; }
+    }
+}
