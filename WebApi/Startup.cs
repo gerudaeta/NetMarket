5d7dd73a9dfee99738c86bb6a437a505592ddc0a
gerudaeta <gerudaeta@gmail.com>
2021-04-05 17:47:16 -0300

    + Add Cors

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -44,6 +44,14 @@ namespace WebApi
             // Inyeccion
             services.AddTransient<IProductoRepository, ProductoRepository>();
             services.AddControllers();
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsRule", rule =>
+                {
+                    rule.AllowAnyHeader().AllowAnyMethod().WithOrigins("*");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,6 +68,8 @@ namespace WebApi
             app.UseStatusCodePagesWithReExecute("/errors", "?code={0}");
 
             app.UseRouting();
+
+            app.UseCors("CorsRule");
 
             app.UseAuthorization();
 
