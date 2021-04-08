e182073c6834af78ff1b630d485ad58ab1e8f291
gerudaeta <gerudaeta@gmail.com>
2021-04-07 23:37:58 -0300

    + Creando usuario seed con UserManager

---
+++ BusinessLogic/Data/SeguridadDbContextData.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace BusinessLogic.Data
+{
+    public class SeguridadDbContextData
+    {
+        private const string passwordUsuario = "german27=";
+            
+        public static async Task SeedUserAsync(UserManager<Usuario> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var usuario = new Usuario
+                {
+                    Nombre = "German",
+                    Apellido = "Udaeta",
+                    UserName = "gerudaeta",
+                    Direccion = new Direccion
+                    {
+                        Calle = "Calle 27",
+                        Ciudad = "Cordoba",
+                        CodigoPostal = "5000",
+                        Departamento = "Cordoba"
+                    }
+                };
+
+                await userManager.CreateAsync(usuario, passwordUsuario);
+            }
+        }
+    }
+}
